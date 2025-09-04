d53cec8bc92b8118be273f94e93db839a378f172
manoharahonnappagoudra@gmail.com <manoharahonnappagoudra@gmail.com>
2025-09-04 15:25:16 +0530

    Fixed navbar styling issue

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,40 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Routes,Route, useLocation } from "react-router-dom"
+import Home from "./pages/Home";
+import Hotel from "./pages/Hotel";
+import Room from "./pages/Room";
+import Singleroom from "./pages/Singleroom";
+import Signup from "./pages/Signup";
+import Login from "./pages/Login";
+import About from "./pages/About";
+import Mybooking from "./pages/Mybooking"
+import Navbar from "./components/Navbar"
+
+
+
+
 
 function App() {
-  const [count, setCount] = useState(0)
+    const ownerPath = useLocation().pathname.includes("owner");
 
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <div>
+     {!ownerPath && <Navbar/>}
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/hotel" element={<Hotel/>}/>
+        <Route path="/room/:id" element={<Room/>}/>
+        <Route path="/singel-room" element={<Singleroom/>}/>
+         <Route path="/signup" element={<Signup/>}/>
+          <Route path="/login" element={<Login/>}/>
+           <Route path="/about" element={<About/>}/>
+            <Route path="/my-booking" element={<Mybooking/>}/>
+         
+        
+
+
+
+      </Routes>
+    </div>
   )
 }
 
